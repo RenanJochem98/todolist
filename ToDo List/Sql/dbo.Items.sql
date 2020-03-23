7d4cdac7899bff475ade1c6b98cd6bf6f1e5caa0
Renan Jochem <renan.jochem@grad.ufsc.br>
2020-03-23 11:44:17 -0300

    Salvando um sql

---
+++ ToDo List/Sql/dbo.Items.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Items] (
+    [ItemId] INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]   NVARCHAR (50) NULL,
+    [UserId] INT           NOT NULL,
+    CONSTRAINT [PK_dbo.Items] PRIMARY KEY CLUSTERED ([ItemId] ASC)
+);
+
